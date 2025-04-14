b89efe264205a38cd3519a5b2887c1a8f7955798
shaheer-git <mshaheer1859@gmail.com>
2025-04-14 08:33:40 +0530

    Comparison and Nullish Coalescing

---
+++ JS/Class-20/js/index.js
@@ -0,0 +1,33 @@
+// Logical
+let a = 10;
+let b = "20";
+let c = 30;
+let bool = true;
+//When both the values are true. && is true.
+(b > a && c > b ) ?
+ console.log(true) : 
+ console.log(false)
+    b = 20;
+if(b > a || c < b){
+    console.log(true)
+} else {
+    console.log(false)
+}
+// if(b > a != a<c ) 
+if(!bool){
+
+} 
+
+//Nullish Coalescing
+
+let name = null;
+let classN = "classname";
+let res = name ?? "Shaheer";
+
+// using or.
+let name1 = 0;
+let classN1 = "classname";
+let re1s = name1 || "Shaheer";
+
+
+
