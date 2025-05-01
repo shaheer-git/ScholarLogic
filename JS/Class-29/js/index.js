bce8b33b2886af0f421f5580f1ea5e7cdc7e3a5a
shaheer-git <mshaheer1859@gmail.com>
2025-05-01 09:00:38 +0530

    Date and Time and JSON and its methods in js

---
+++ JS/Class-29/js/index.js
@@ -0,0 +1,8 @@
+// Date and Time.
+let date = new Date();
+console.log(date)
+// please practice date methods.
+
+// JSON
+const jsonString = '{"name": "Alice", "age": 25}';
+console.log(isRowJSON(jsonString)); // true
